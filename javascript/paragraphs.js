63426e3dfa3c512672f3648e2f41e82bbc063f35
Swetha Nagendran <89972180+swe-143@users.noreply.github.com>
2022-01-01 14:04:50 +0530

    Add files via upload

---
+++ javascript/paragraphs.js
@@ -0,0 +1,23 @@
+const paragraphs = [
+  "Industries get the main portion of their raw materials from such natural resources in the form of agricultural goods, semi-finished products, and sometimes from the ground in the form of fossil fuels and precious metals.",
+  "Each country has its points in nature, where it is abundant and gifted. For example, the Middle East is abundant in fossil fuels, the African continent in wildlife, and North America in varied landforms",
+  "It is fundamentally important for countries to understand that management and sustainable development of these resources is essential for life to prevail on the planet.",
+  "Resources that are finite and non-renewable should especially be considered in sustainable development as once depleted, and they would take thousands of years to replenish.",
+  "Many Natural Resource management practices have come into the picture and faded over the years. Some are practised even today, others have become redundant with the development of technology.",
+  "Currently, Information Technology is a hot hub for natural resource management as we increase the rate of integration of nature and technology. IT is being used at a large scale to monitor, study, and understand our natural resources- especially the finite ones.",
+  "Now, a lot of emphases is put on technology, more specifically geospatial technology and remote sensing for the management of Natural resources.",
+  "GIS and remote sensing provide data and information that can provide a strong basis for decision making in terms of sustainable development and growth and preservation of natural resources.",
+  "These nascent technologies are mainly used for mapping. This does not keep it limited to our day to day GPS mapping, but it includes a variety of maps such as vegetation mapping, mapping by soil type, land cover maps, and even geology maps.",
+  "A vast range of informative data is collected before these maps are drafted. This data is used to generate information, patterns, and back researches and decisions that help in sustainable development and preservation of natural resources.",
+  "The elements of the study are derived through various methods such as aerial photographs, land photographs, and satellite imaging, but the type of photography is decided by the aim and reason behind the study.",
+  "GIS and remote sensing are crucial elements of forest management as well. This technology helps understand the land cover, availability, area, and human encroachment of protected forest areas.",
+  "It is used in watershed management, which is crucial for understanding the water streams and integration of water resources available, in studying and preventing desertification of areas, and even in biodiversity management.",
+  "These are just a few of the areas where Geospatial Information and remote sensing are used to combat issues in natural resources, their management, and development.",
+  "Although the populations are rising and the pressure on resources is increasing by the day, with GIS, there is hope that this pressure can be reduced- and if not reduced, at least it can be studied to find areas where it is beyond the threshold.",
+  "These are just a few of the areas where Geospatial Information and remote sensing are used to combat issues in natural resources, their management, and development.",
+  "Although the populations are rising and the pressure on resources is increasing by the day, with GIS, there is hope that this pressure can be reduced- and if not reduced, at least it can be studied to find areas where it is beyond the threshold.",
+  "GIS in natural resource management is used in the reduction of a natural hazard such as flood, landslide, soil erosion, forest fires, earthquake, drought etc.",
+  "One cannot totally stop these natural disasters but can minimize these warnings by early planning, preparation, and strategies. GIS in natural resource management is being used in analyzing, organizing, managing and monitoring the natural hazards.",
+  "GIS in natural resource management provides spatial data of the disasters that have taken place before or might to occur so that early risk can be prevented. It is indicated through the GIS-based map."
+];
+
